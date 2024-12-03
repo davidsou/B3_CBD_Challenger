5bf629e7aaa447c4bd917727d034c077055e2c1e
David Oliveira <90005502@pmenos.com.br>
2024-12-02 21:14:16 -0300

    ajuste de formatação.

--- B3_CBD_Challenger.Application/Validators/InvestmentSimulationRequestValidator.cs
+++ B3_CBD_Challenger.Application/Validators/InvestmentSimulationRequestValidator.cs
@@ -1,25 +1,18 @@
 ﻿using B3_CBD_Challenger.Application.Model;
 using FluentValidation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace B3_CBD_Challenger.Application.Validators
+namespace B3_CBD_Challenger.Application.Validators;
+public class InvestmentSimulationRequestValidator:AbstractValidator<InvestimentSimulationRequest>
 {
-    public class InvestmentSimulationRequestValidator:AbstractValidator<InvestimentSimulationRequest>
+    public InvestmentSimulationRequestValidator()
     {
-        public InvestmentSimulationRequestValidator()
-        {
-            RuleFor(x => x.InitialValue)
-            .NotNull().WithMessage("InitialValue is required")
-            .GreaterThan(0).WithMessage("InitialValue must be greather than zero");
+        RuleFor(x => x.InitialValue)
+        .NotNull().WithMessage("InitialValue is required")
+        .GreaterThan(0).WithMessage("InitialValue must be greather than zero");
 
-            RuleFor(x => x.Months)
-            .NotNull().WithMessage("Months is required")
-            .GreaterThan(1).WithMessage(errorMessage: "Amount of months must be greather than 1");
-                
-        }
+        RuleFor(x => x.Months)
+        .NotNull().WithMessage("Months is required")
+        .GreaterThan(1).WithMessage(errorMessage: "Amount of months must be greather than 1");
+            
     }
 }
